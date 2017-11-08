9c819988d8258be933699e514be2c3e18b3dc5b9
Kabir Sondhi <ksondhi78@gmail.com>
2017-11-07 23:00:12 -0500

    Modified oracle docker image to be installed

--- scripts/install_oracle_docker_image.sh
+++ scripts/install_oracle_docker_image.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing Docker image with Oracle..."
-sudo docker pull oraclelinux
-sudo docker run -dit oraclelinux
+sudo docker pull konnecteam/oracle12c-database
+sudo docker run -dit -p 8080:8080 -p 1521:152 konnecteam/oracle12c-database
 sudo docker ps
